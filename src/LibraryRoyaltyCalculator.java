cb6699f08abfb1c031528eec204f211a4bbb267a
JonathanKudsk <jonathankudsk@gmail.com>
2024-11-01 09:37:33 +0100

    SP2 Opgave

--- src/LibraryRoyaltyCalculator.java
+++ src/LibraryRoyaltyCalculator.java
@@ -4,15 +4,15 @@ public class LibraryRoyaltyCalculator {
     public static void main(String[] args) {
         ArrayList<Title> titles = new ArrayList<>();
 
-        Author author1 = new Author( "H.C Andersen");
-        Title printedBook1 = new PrintedBook("Den lille havfrue", "SKØN", 1000, 500);
-        Title printedBook2 = new PrintedBook("Den grimme ælling", "BI", 2000, 250);
-        Title audioBook1 = new AudioBook("Det flyvende tæppe", "LYRIK", 3000, 125);
+        Author author1 = new Author( "Jonathan Kudsk");
+        Title printedBook1 = new PrintedBook("Abdi på en skuter", "BI", 1000, 500);
+        Title printedBook2 = new PrintedBook("Eren i bakken", "SKØN", 2000, 250);
+        Title audioBook1 = new AudioBook("2 Sjakaler på togt", "LYRIK", 3000, 125);
 
         author1.addTitle(printedBook1);
         author1.addTitle(printedBook2);
         author1.addTitle(audioBook1);
 
-        System.out.println(author1.getName() + " " + author1.calculateTotalPay()+"kr");
+        System.out.println(author1.getName() + ": " + author1.calculateTotalPay()+" kr");
     }
 }
